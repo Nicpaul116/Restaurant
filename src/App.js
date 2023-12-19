fb415d4a3f2828c78e1c72fb1e9f09569e093bcb
Nicpaul Gacha <nicpaulgacha@gmail.com>
2023-12-19 22:56:38 +0100

    Restaurant

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Blog, Footer, Header, Menu, Contact, Login } from "./containers";
 import { Navbar } from "./components";
-import "./app.css";
+import "./App.css";
 
 const App = () => {
   return (
